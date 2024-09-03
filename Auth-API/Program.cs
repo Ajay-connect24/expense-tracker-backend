5b930cbe1dda4da271d9f715070977c8821f78d8
Ajay.K <Ajay.K@ust>
2024-09-03 16:04:03 +0530

    add db services and add migrations

--- Auth-API/Program.cs
+++ Auth-API/Program.cs
@@ -1,12 +1,30 @@
+using Auth_API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("ConnStr"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthDbContext>()
+.AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+
+
 
 app.UseAuthorization();
 
