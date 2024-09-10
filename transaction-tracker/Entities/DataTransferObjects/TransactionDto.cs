1c83955f55e109e069f2271e5e82c0a083e4c333
Ajay.K <Ajay.K@ust>
2024-09-10 13:05:24 +0530

    Add models and dtos

---
+++ transaction-tracker/Entities/DataTransferObjects/TransactionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace transaction_tracker.Entities.DataTransferObjects
+{
+    public class TransactionDto
+    {
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public string? Note { get; set; } = null;
+    }
+}
---
+++ transaction-tracker/Entities/Models/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace transaction_tracker.Entities.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public string? Note { get; set; }
+    }
+}
