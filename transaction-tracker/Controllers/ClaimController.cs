cb7994113799c5222b1f8c7def389397f52c7081
Ajay.K <Ajay.K@ust>
2024-09-12 12:20:59 +0530

    modified token as json and add ClaimController

---
+++ transaction-tracker/Controllers/ClaimController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Mapster;
+using transaction_tracker.Entities.Models;
+
+namespace transaction_tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClaimController : ControllerBase
+    {
+        [HttpGet("userDetails")]
+        public ActionResult<UserDetailsDto> GetUserDetailsFromClaims()
+        {
+            // Retrieve the claims from the JWT token
+            var claimsIdentity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (claimsIdentity == null)
+            {
+                return Unauthorized();
+            }
+
+            // Extract claims
+            var claims = claimsIdentity.Claims;
+
+            // Create a UserDetailsDto from claims
+            var userDetailsDto = new UserDetailsDto
+            {
+                Id = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value,
+                UserName = claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value,
+                Email = claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value
+            };
+
+            // Return the UserDetailsDto
+            return Ok(userDetailsDto);
+        }
+    }
+}
